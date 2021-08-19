ab936d75d2eefa861b405044599ec68f5da64ba3
xavigu <xavigu91@gmail.com>
2021-08-19 15:01:48 +0200

    update the iframe with an html template by default and with the content of the textareas, and add some styles

--- main.js
+++ main.js
@@ -5,17 +5,39 @@ const getEl = selector => document.querySelector(selector)
 const $html = getEl('#html')
 const $css = getEl('#css')
 const $js = getEl('#js')
+const $iframe = getEl('iframe')
 
-$html.addEventListener('input', e => {
-  console.log(e.target.value)
-})
+$html.addEventListener('input', update)
 
-$js.addEventListener('input', e => {
-  console.log(e.target.value)
-})
+$js.addEventListener('input', update)
 
-$css.addEventListener('input', e => {
-  console.log(e.target.value)
-})
+$css.addEventListener('input', update)
 
+function update() {
+  const html = createHtml()
+  $iframe.setAttribute('srcdoc', html)
+}
 
+const createHtml = () => {
+  const html = $html.value;
+  const js = $js.value;
+  const css = $css.value;
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <style>
+        ${css}
+      </style>
+    </head>
+    <body>
+      <script>
+        ${js}
+      </script>
+      ${html}
+    </body>
+  </html>
+`
+
+}
